ffaf56505f99f7de9525026cbca809ecb3f515fa
iondune <iondune@localhost>
2012-01-23 04:27:53 -0800

    + Removed references to deprecated files.

--- CabbageCollider/Utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UTILS_H_INCLUDED_
-#define _UTILS_H_INCLUDED_
-
-bool const equals(float const a, float const b, float const epsilon = 0.00001f)
-{
-	return fabsf(a) <= epsilon;
-}
-
-#endif
