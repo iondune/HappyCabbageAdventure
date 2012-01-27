d7f75d3bb61d9051e7e9b00afd1b3f15dd1beb72
iondune <iondune@localhost>
2012-01-27 02:27:10 -0800

    + Added CabbageScene.h file for including all files

---
+++ CabbageScene/CabbageScene.h
@@ -0,0 +1,12 @@
+#ifndef _CABBAGE_SCENE_H_INCLUDED_
+#define _CABBAGE_SCENE_H_INCLUDED_
+
+#include "CMesh.h"
+#include "CMeshLoader.h"
+#include "CRenderable.h"
+#include "CShader.h"
+#include "CTexture.h"
+#include "CTextureLoader.h"
+#include "SVertex.h"
+
+#endif
