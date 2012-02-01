e4f911ce8aaa3754b7d455f3de41725503b64684
iondune <iondune@localhost>
2012-01-31 19:36:50 -0800

    + Spaces -> tab

--- CabbageFramework/CStateManager.cpp
+++ CabbageFramework/CStateManager.cpp
@@ -1,33 +1,33 @@
 #include "CStateManager.h"
 
 CStateManager::CStateManager()
-	: CurrentState(0)
+    : CurrentState(0)
 {}
 
 
 void CStateManager::OnGameTickStart(float const Elapsed)
 {
-	if (CurrentState)
-		CurrentState->OnGameTickStart(Elapsed);
+    if (CurrentState)
+        CurrentState->OnGameTickStart(Elapsed);
 }
 
 void CStateManager::OnGameTickEnd(float const Elapsed)
 {
-	if (CurrentState)
-		CurrentState->OnGameTickEnd(Elapsed);
+    if (CurrentState)
+        CurrentState->OnGameTickEnd(Elapsed);
 }
 
 
 void CStateManager::OnRenderStart(float const Elapsed)
 {
-	if (CurrentState)
-		CurrentState->OnRenderStart(Elapsed);
+    if (CurrentState)
+        CurrentState->OnRenderStart(Elapsed);
 }
 
 void CStateManager::OnRenderEnd(float const Elapsed)
 {
-	if (CurrentState)
-		CurrentState->OnRenderEnd(Elapsed);
+    if (CurrentState)
+        CurrentState->OnRenderEnd(Elapsed);
 }
 
 void CStateManager::OnMouseEvent(SMouseEvent const & Event)
