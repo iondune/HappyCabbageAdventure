12e3b251fe43d173f7aa3331b664213449ef6d9e
Ian <Ian@localhost>
2012-02-04 16:50:26 -0800

    Merge fix

--- base/CGameEventReceiver.cpp
+++ base/CGameEventReceiver.cpp
@@ -13,7 +13,7 @@ void CGameEventReceiver::OnEnemyDeath(SEnemyDeathEvent const & Event) {
     fprintf(stderr, "Removing enemy %d\n", Event.Enemy);
     fprintf(stderr, "Removing renderable %d\n", Event.Renderable);
 
-    //CApplication::get().getSceneManager().removeRenderable(Event.Renderable);
+    CApplication::get().getSceneManager().removeRenderable(Event.Renderable);
     
     numKilled++;
 }
