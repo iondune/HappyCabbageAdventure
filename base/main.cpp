93a768b8201f9f8eb546be5cbf9b103da0ba3eed
Alden Tai <xtoken.x@gmail.com>
2012-02-07 15:02:36 -0800

    add main

---
+++ base/main.cpp
@@ -0,0 +1,14 @@
+#include "CGameState.h"
+
+int main(int argc, char *argv[])
+{
+   CApplication & Application = Application.get();
+   Application.init(SPosition2(1024, 768));
+
+   Application.getStateManager().setState<CGameState>();
+
+   Application.run();
+
+   return 0;
+}
+
