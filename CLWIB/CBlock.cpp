f49190311b0d757370adfffdba3af3856e898d00
Alden Tai <xtoken.x@gmail.com>
2012-02-07 22:18:07 -0800

    added CBlock and CPlaceable

---
+++ CLWIB/CBlock.cpp
@@ -0,0 +1,11 @@
+#include "CBlock.h"
+
+CBlock::CBlock(float nx, float ny, int width, int height) : 
+   x(nx), y(ny), w(width), h(height) {
+   }
+
+void CBlock::printXML() {
+   printf("CBlock printXML function unimplemented\n");
+}
+void CBlock::moveTo(float x,float y) {
+}
