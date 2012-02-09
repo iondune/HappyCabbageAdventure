a58912af54d780ceecd8b20c6a202255ec2a9708
JorgeMendoza <JorgeMendoza@JorgeMendoza-PC.wireless.calpoly.edu>
2012-02-08 21:30:41 -0800

    + added stuff for the level loader via enemies

--- CLWIB/CPlaceable.h
+++ CLWIB/CPlaceable.h
@@ -2,6 +2,13 @@
 #define __CPLACEABLE_H____
 #include <sstream>
 #include <string>
+#include "../CabbageScene/CabbageScene.h"
+#include "../CabbageFramework/CabbageFramework.h"
+#include "../base/CGameplayManager.h"
+#include "../CabbageCollider/CEngine.h"
+#include "../CabbageCollider/CObject.h"
+#include "../CabbageCollider/CActor.h"
+
 class CPlaceable {
    public:
    float x,y;
@@ -10,6 +17,7 @@ class CPlaceable {
       virtual void printXML() =0;
       virtual void moveTo(float,float) =0;
       virtual std::string tag();
+      virtual CMeshRenderable* setupItem(CShader*, Cabbage::Collider::CEngine*, CGameplayManager*);
       std::string getX();
       std::string getY();
       std::string getWidth();
