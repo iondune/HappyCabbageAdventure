c53835db6eeb71e4a1ea989aa8253fcfcb07311a
iondune <iondune@localhost>
2012-02-08 18:45:14 -0800

    + More of previous

--- MeshLoaderDemo/Main.cpp
+++ MeshLoaderDemo/Main.cpp
@@ -46,7 +46,7 @@ public:
     void begin()
     {
         // OpenGL init
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LEQUAL);
