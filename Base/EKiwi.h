54bdd6075e005acf826d2f44d602e216664412e4
ccolwell <ccolwell@localhost>
2012-02-19 18:48:46 -0800

    Forgot actual Kiwi AI.

---
+++ Base/EKiwi.h
@@ -0,0 +1,27 @@
+/*
+ * EKiwi.h
+ *
+ *  Created on: Feb 19, 2012
+ *      Author: Klonoa
+ */
+
+#ifndef EKIWI_H_
+#define EKIWI_H_
+
+#include "CBadGuy.h"
+#include "CGameplayManager.h"
+
+class EKiwi : public CBadGuy
+{
+
+public:
+   EKiwi(float x, float y, float w, float h, CGameplayManager* manager);
+
+   void update(const float TickTime);
+   void loadMesh();
+   void loadActor();
+
+   float OrigX;
+};
+
+#endif /* EKIWI_H_ */
