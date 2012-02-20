97a45664df3a67c2adfa1ceb219e69d3ee0ab15c
JorgeMendoza <JorgeMendoza@JorgeMendoza-PC>
2012-02-19 20:41:08 -0800

    + added CPlaceable stuff for Cabbage in the mean time need to add it to   make file + the CCabbage.h and .cpp do not touch the main code ATM so the current build   should still run fine.

---
+++ StateLWIB/CCabbage.cpp
@@ -0,0 +1,35 @@
+#include "CCabbage.h"
+
+CCabbage::CCabbage(float nx, float ny, int width, int height)
+{
+   x = nx; y = ny;
+   w = width; h = height;
+   isMovingPlatform = 0;
+}
+void CCabbage::writeXML(xmlwriter *l) {
+    std::stringstream xValue, yValue, widthValue, heightValue, tagValue, isMovingValue;
+    xValue << x;
+    yValue << y;
+    widthValue << w;
+    heightValue << h;
+    isMovingValue << isMovingPlatform;
+
+    tagValue << "CCabbage";
+
+    l->AddAtributes("isMoving ", isMovingValue.str());
+    l->AddAtributes("width ", widthValue.str());
+    l->AddAtributes("height ", heightValue.str());
+    l->AddAtributes("Y ", yValue.str());
+    l->AddAtributes("X ", xValue.str());
+    l->Createtag(tagValue.str());
+    l->CloseLasttag();
+}
+void CCabbage::moveTo(float x,float y) {
+   //For usage in LWIB
+}
+
+CMeshRenderable * CCabbage::setupItem(CShader * shader, Cabbage::Collider::CEngine *Engine, CGameplayManager *GameplayManager , Cabbage::Collider::CActor *Player){
+    Player = Engine->addActor();
+    Player->setArea(SRect2(CPlaceable::x,y,(float)w,(float)h));
+    return new CMeshRenderable(); // not sure to really return here
+}
---
+++ StateLWIB/CCabbage.h
@@ -0,0 +1,26 @@
+#ifndef __CCABBAGE_H____
+#define __CCABBAGE_H____
+
+#include <stdio.h>
+#include "CPlaceable.h"
+#include "../CabbageScene/CabbageScene.h"
+#include "../CabbageFramework/CabbageFramework.h"
+#include "../Base/CGameplayManager.h"
+#include "../CabbageCollider/CEngine.h"
+#include "../CabbageCollider/CObject.h"
+#include "../CabbageCollider/CActor.h"
+#include <sstream>
+#include <string>
+class CCabbage : public CPlaceable
+{
+   CShader* shader;
+   public:
+   int num;
+   CCabbage(float nx, float ny, int width, int height);
+   void writeXML(xmlwriter *);
+   void moveTo(float,float);
+   void setShader(CShader*);
+   CMeshRenderable * setupItem(CShader * shader, Cabbage::Collider::CEngine *Engine, CGameplayManager *GameplayManager, Cabbage::Collider::CActor *Player);
+};
+
+#endif
