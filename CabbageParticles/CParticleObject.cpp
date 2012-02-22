b91924970a8c1b87bb10f3ea114b2327062af3b9
Alden Tai <xtoken.x@gmail.com>
2012-02-21 22:43:02 -0800

    particle updates

--- CabbageParticles/CParticleObject.cpp
+++ CabbageParticles/CParticleObject.cpp
@@ -5,10 +5,26 @@
 
 #include "../CabbageCore/glm/gtc/matrix_transform.hpp"
 CRenderable * CParticleObject::getParticlesRenderable() {return particlesRenderable;}
-void CParticleObject::setPositions(SVector3 *vectorArr) {
+void CParticleObject::setPositions(std::vector<SVector3*> vectorArr) {
    updated = false;
    positionsArr = vectorArr;
 }
+
+static inline bool printOpenGLErrors()
+{
+    bool Succeeded = true;
+
+    GLenum glErr = glGetError();
+    while (glErr != GL_NO_ERROR)
+    {
+        std::cerr << "OpenGL Error: " << gluErrorString(glErr) << std::endl;
+        Succeeded = false;
+        glErr = glGetError();
+    }
+
+    return Succeeded;
+}
+
 
 /*
 void CMeshSceneObject::update()
@@ -27,12 +43,13 @@ void CParticleObject::update() {
    PositionBuffer.clear();
    for(int i = 0; i < numParticles; i++) {
       for(int j = 0; j < 3; j++) {
-         PositionBuffer.push_back(positionsArr[i][j]);
+         PositionBuffer.push_back((*(positionsArr[i]))[j]);
       }
    }
 }
 
-void CParticleObject::setup(SVector3 * vectorArr, SVector3 * colorArr, float * sizeArr, int num) {
+void CParticleObject::setup(std::vector<SVector3*> vectorArr, std::vector<SVector3*> colorArr, std::vector<float> sizeArr, int num) {
+   printf("Beginning of setup: "); printOpenGLErrors();
    updated = true;
    numParticles = num;
    particlesRenderable = new CRenderable(this);
@@ -40,8 +57,8 @@ void CParticleObject::setup(SVector3 * vectorArr, SVector3 * colorArr, float * s
    positionsArr = vectorArr;
    for(int i = 0; i < numParticles; i++) {
       for(int j = 0; j < 3; j++) {
-         PositionBuffer.push_back(vectorArr[i][j]);
-         ColorBuffer.push_back(colorArr[i][j]);
+         PositionBuffer.push_back((*(vectorArr[i]))[j]);
+         ColorBuffer.push_back((*(colorArr[i]))[j]);
          SizeBuffer.push_back(sizeArr[i]);
       }
    }
@@ -57,6 +74,8 @@ void CParticleObject::setup(SVector3 * vectorArr, SVector3 * colorArr, float * s
 
    // Add mesh index buffer
    particlesRenderable->setIndexBufferObject(& IndexBuffer);
+   particlesRenderable->setDrawType(GL_POINTS);
+   particlesRenderable->Size = num;
 
    // Reset the shader to load attributes again
    particlesRenderable->reloadVariablesOnNextDraw();
