1d12bf5bac773d9b89b4facb87973f2a27ea1cba
Alden Tai <xtoken.x@gmail.com>
2012-02-23 11:50:46 -0800

    Moved Jorge's level to the first level

--- StateOverworld/COverworldState.cpp
+++ StateOverworld/COverworldState.cpp
@@ -259,9 +259,9 @@ void COverworldState::end()
 void COverworldState::loadLevels()
 {
   //If you're adding more levels be sure to update COverworldState's NUM_LEVELS
-  levels[0].name = "chris.xml";
+  levels[0].name = "jorge1.xml";
   levels[0].loc = SVector3(0.33, -0.17, 1.05); //bit of green near sole orange hill
-  levels[1].name = "jorge1.xml";
+  levels[1].name = "chris.xml";
   levels[1].loc = SVector3(0.83, 0.00, 0.65); //Green hill
   levels[2].name = "test.xml";
   levels[2].loc = SVector3(0.9f, -0.12999999f, 0.3f);
