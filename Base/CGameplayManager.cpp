a79546c44fd4d8c8f0bd6abcfcec363b564423bd
Alden Tai <xtoken.x@gmail.com>
2012-02-27 18:47:26 -0800

    add impulse for jumping on an enemy

--- Base/CGameplayManager.cpp
+++ Base/CGameplayManager.cpp
@@ -87,6 +87,8 @@ void CGameplayManager::OnCollision(Cabbage::Collider::CCollideable * Object, Cab
                 //fprintf(stderr, "Enemy detected as dead! %d\n", it->Renderable);
 
                 Enemies.erase(it);
+
+                PlayerActor->setImpulse(SVector2(0.f, 1.0f), 0.05);
             }
             //Need to rewrite so works without SEnemy
             else
