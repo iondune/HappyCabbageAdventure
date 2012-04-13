7525f45c8d61c66b5338af2915baf0bffc855f75
iondune <iondune@localhost>
2012-04-12 18:08:14 -0700

    + ISceneObject reformat

--- CabbageScene/ISceneObject.cpp
+++ CabbageScene/ISceneObject.cpp
@@ -9,13 +9,13 @@
 
 
 ISceneObject::ISceneObject()
-    : DebugDataFlags(0), Visible(true), Parent(0), UseCulling(true), Immobile(false)
+	: DebugDataFlags(0), Visible(true), Parent(0), UseCulling(true), Immobile(false)
 {}
 
 void ISceneObject::updateAbsoluteTransformation()
 {
-   if(Immobile)
-      return;
+	if(Immobile)
+		return;
 	AbsoluteTransformation = Transformation;
 	if (Parent)
 	{
@@ -35,7 +35,7 @@ glm::mat4 const & ISceneObject::getAbsoluteTransformation() const
 
 void ISceneObject::setTranslation(SVector3 const & translation)
 {
-   assert(!Immobile);
+	assert(!Immobile);
 	Translation = translation;
 	Transformation.setTranslation(translation);
 }
@@ -53,15 +53,15 @@ void ISceneObject::setRotation(glm::mat4 const & matrix)
 
 void ISceneObject::setScale(SVector3 const & scale)
 {
-   //assert(!Immobile);
+	//assert(!Immobile);
 	Scale = scale;
 	Transformation.setScale(scale);
 }
 
 void ISceneObject::update()
 {
-   if(Immobile)
-      return;
+	if(Immobile)
+		return;
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it)
 		(* it)->update();
 }
@@ -76,34 +76,34 @@ void ISceneObject::draw(CScene const * const scene, ERenderPass const Pass)
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it) {
 		if (! (* it)->isCulled(scene)) {
 			(* it)->draw(scene, Pass);
-      }
-      else {
-         /*
-         if((* it)->isImmobile())
-            printf("Culled an immobile ISO\n");
-         */
-         numCulled++;
-      }
-      numObjects++;
-   }
+		}
+		else {
+			/*
+			if((* it)->isImmobile())
+			printf("Culled an immobile ISO\n");
+			*/
+			numCulled++;
+		}
+		numObjects++;
+	}
 }
 
 SBoundingBox3 const & ISceneObject::getBoundingBox() const
 {
-    return BoundingBox;
+	return BoundingBox;
 }
 
 SBoundingBox3 & ISceneObject::getBoundingBox()
 {
-    return BoundingBox;
+	return BoundingBox;
 }
 void ISceneObject::setBoundingBox(SBoundingBox3 const & boundingBox) {
-   BoundingBox = boundingBox;
+	BoundingBox = boundingBox;
 }
 
 bool const ISceneObject::isDebugDataEnabled(EDebugData::Domain const type) const
 {
-    return (type & DebugDataFlags) != 0;
+	return (type & DebugDataFlags) != 0;
 }
 
 void ISceneObject::enableDebugData(EDebugData::Domain const type)
@@ -111,10 +111,10 @@ void ISceneObject::enableDebugData(EDebugData::Domain const type)
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it)
 		(* it)->enableDebugData(type);
 
-    if (type == EDebugData::None)
-        DebugDataFlags = 0;
-    else
-        DebugDataFlags |= type;
+	if (type == EDebugData::None)
+		DebugDataFlags = 0;
+	else
+		DebugDataFlags |= type;
 }
 
 void ISceneObject::disableDebugData(EDebugData::Domain const type)
@@ -122,25 +122,25 @@ void ISceneObject::disableDebugData(EDebugData::Domain const type)
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it)
 		(* it)->disableDebugData(type);
 
-    if (type == EDebugData::None)
-        DebugDataFlags = 0;
-    else
-        DebugDataFlags ^= type;
+	if (type == EDebugData::None)
+		DebugDataFlags = 0;
+	else
+		DebugDataFlags ^= type;
 }
 
 bool const ISceneObject::intersectsWithLine(SLine3 const & line) const
 {
-    return BoundingBox.intersectsWithLine(line);
+	return BoundingBox.intersectsWithLine(line);
 }
 
 bool const ISceneObject::isVisible() const
 {
-    return Visible;
+	return Visible;
 }
 
 void ISceneObject::setVisible(bool const isVisible)
 {
-    Visible = isVisible;
+	Visible = isVisible;
 }
 
 STransformation3 const & ISceneObject::getTransformation() const
@@ -202,14 +202,14 @@ SVector3 const & ISceneObject::getScale() const
 
 bool const ISceneObject::isCulled(CScene const * const Scene) const
 {
-   static bool const Inside = false;
-   static bool const Outside = true;
-   timesCalled++;
+	static bool const Inside = false;
+	static bool const Outside = true;
+	timesCalled++;
 
-    if (! UseCulling || ! Scene->isCullingEnabled())
-        return false;
+	if (! UseCulling || ! Scene->isCullingEnabled())
+		return false;
 
-    int i = 0;
+	int i = 0;
 	int in[6], out[6];
 
 	for (int i = 0; i < 6; ++ i) 
@@ -218,21 +218,21 @@ bool const ISceneObject::isCulled(CScene const * const Scene) const
 		out[i] = 0;
 	}
 
-    for (int i = 0; i < 8; ++ i) 
+	for (int i = 0; i < 8; ++ i) 
 	{
-        SVector3 const Center = getBoundingBox().getCorner(i);
-        glm::vec4 Center4(Center.X, Center.Y, Center.Z, 1.f);
+		SVector3 const Center = getBoundingBox().getCorner(i);
+		glm::vec4 Center4(Center.X, Center.Y, Center.Z, 1.f);
 
-        glm::mat4 PVM;
-        if(!Immobile)
-           PVM = (Scene->getActiveCamera()->getProjectionMatrix()*Scene->getActiveCamera()->getViewMatrix()*Transformation());
-        else
-           PVM = Scene->getActiveCamera()->getProjectionMatrix()*Scene->getActiveCamera()->getViewMatrix()*glm::mat4(1.);
-        glm::vec4 prime = PVM * Center4;
+		glm::mat4 PVM;
+		if(!Immobile)
+			PVM = (Scene->getActiveCamera()->getProjectionMatrix()*Scene->getActiveCamera()->getViewMatrix()*Transformation());
+		else
+			PVM = Scene->getActiveCamera()->getProjectionMatrix()*Scene->getActiveCamera()->getViewMatrix()*glm::mat4(1.);
+		glm::vec4 prime = PVM * Center4;
 
-        float length = glm::length(glm::vec3(prime.x, prime.y, prime.z));
+		float length = glm::length(glm::vec3(prime.x, prime.y, prime.z));
 
-        if (-prime.w < prime.x)
+		if (-prime.w < prime.x)
 			in[0] ++;
 		else
 			out[0] ++;
@@ -242,17 +242,17 @@ bool const ISceneObject::isCulled(CScene const * const Scene) const
 		else
 			out[1] ++;
 
-        if (-prime.w < prime.y)
+		if (-prime.w < prime.y)
 			in[2] ++;
 		else
 			out[2] ++;
- 
+
 		if (prime.w > prime.y)
 			in[3] ++;
 		else
 			out[3] ++;
 
-        if (-prime.w < prime.z)
+		if (-prime.w < prime.z)
 			in[4] ++;
 		else
 			out[4] ++;
@@ -261,7 +261,7 @@ bool const ISceneObject::isCulled(CScene const * const Scene) const
 			in[5] ++;
 		else
 			out[5] ++;
-    }
+	}
 
 	for (int i = 0; i < 6; ++ i) 
 	{
@@ -270,45 +270,45 @@ bool const ISceneObject::isCulled(CScene const * const Scene) const
 		else if (out[i])
 			return Inside;
 	}
-   return Inside;
+	return Inside;
 }
 
 bool ISceneObject::isImmobile() {
-   return Immobile;
+	return Immobile;
 }
 void ISceneObject::setImmobile(bool val) {
-   Immobile = val;
+	Immobile = val;
 }
 
 SVector3 ISceneObject::getWorldBoundingBoxMinPoint() {
-   if(!Immobile) {
-      SVector3 p = getBoundingBox().MinCorner; 
-      glm::vec4 p4(p.X, p.Y, p.Z, 1.f);
-      glm::vec4 temp = Transformation() * p4; 
+	if(!Immobile) {
+		SVector3 p = getBoundingBox().MinCorner; 
+		glm::vec4 p4(p.X, p.Y, p.Z, 1.f);
+		glm::vec4 temp = Transformation() * p4; 
 
-      //printf("Min: %0.2f %0.2f %0.2f\n", temp.x, temp.y, temp.z);
-      return SVector3(temp.x, temp.y, temp.z);
-   }
-   else
-      return getBoundingBox().MinCorner;
+		//printf("Min: %0.2f %0.2f %0.2f\n", temp.x, temp.y, temp.z);
+		return SVector3(temp.x, temp.y, temp.z);
+	}
+	else
+		return getBoundingBox().MinCorner;
 }
 
 SBoundingBox3 ISceneObject::getWorldBoundingBox() {
-   if(!Immobile) {
-      SVector3 min = getBoundingBox().MinCorner; 
-      glm::vec4 min4(min.X, min.Y, min.Z, 1.f);
-      glm::vec4 temp = Transformation() * min4; 
-      //printf("Min: %0.2f %0.2f %0.2f\n", temp.x, temp.y, temp.z);
+	if(!Immobile) {
+		SVector3 min = getBoundingBox().MinCorner; 
+		glm::vec4 min4(min.X, min.Y, min.Z, 1.f);
+		glm::vec4 temp = Transformation() * min4; 
+		//printf("Min: %0.2f %0.2f %0.2f\n", temp.x, temp.y, temp.z);
 
-      SVector3 max = getBoundingBox().MaxCorner; 
-      glm::vec4 max4(max.X, max.Y, max.Z, 1.f);
-      glm::vec4 temp2 = Transformation() * max4; 
-      //printf("Max: %0.2f %0.2f %0.2f\n", temp2.x, temp2.y, temp2.z);
+		SVector3 max = getBoundingBox().MaxCorner; 
+		glm::vec4 max4(max.X, max.Y, max.Z, 1.f);
+		glm::vec4 temp2 = Transformation() * max4; 
+		//printf("Max: %0.2f %0.2f %0.2f\n", temp2.x, temp2.y, temp2.z);
 
-      return SBoundingBox3(SVector3(temp.x, temp.y, temp.z), SVector3(temp2.x, temp2.y, temp2.z));
-   }
-   else
-      return getBoundingBox();
+		return SBoundingBox3(SVector3(temp.x, temp.y, temp.z), SVector3(temp2.x, temp2.y, temp2.z));
+	}
+	else
+		return getBoundingBox();
 }
 
 
@@ -329,17 +329,17 @@ void ISceneObject::load(CScene const * const Scene, ERenderPass const Pass)
 }
 
 void ISceneObject::setTreeImmobile(bool value) {
-   Immobile = value;
+	Immobile = value;
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it)
 		(* it)->setTreeImmobile(value);
 }
 
 int ISceneObject::getNumLeaves() {
-   if(Children.size() == 0)
-      return 1;
-   int toRet = 0;
+	if(Children.size() == 0)
+		return 1;
+	int toRet = 0;
 	for (std::list<ISceneObject *>::iterator it = Children.begin(); it != Children.end(); ++ it) {
-      toRet += (* it)->getNumLeaves();
-   }
-   return toRet;
+		toRet += (* it)->getNumLeaves();
+	}
+	return toRet;
 }
