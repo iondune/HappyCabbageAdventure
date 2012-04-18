78c781eb82f443f205d420d8f8e3046c5a9ea3cc
Alden Tai <xtoken.x@gmail.com>
2012-04-18 04:00:54 -0700

    moving some code around

--- Base/CGameplayManager.cpp
+++ Base/CGameplayManager.cpp
@@ -1,5 +1,12 @@
 #include "CGameplayManager.h"
 
+CGameplayManager::CGameplayManager(CGameLevel & level) {
+   PhysicsEngine = new CEngine();
+   for(int i = 0; i < level.getPlaceables().size(); i++) {
+      
+   }
+}
+
 void CGameplayManager::update(float time) {
    PhysicsEngine->update(time);
    for(int i = 0; i < Elements.size(); i++) {
