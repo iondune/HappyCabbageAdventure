1c411c8eaaad875c7bfeab1619432f1ec5c4143b
Alden Tai <xtoken.x@gmail.com>
2012-04-18 23:57:46 -0700

    started adding in the camera/light code. will find a better spot

--- StateGame/CGameState.cpp
+++ StateGame/CGameState.cpp
@@ -5,12 +5,22 @@ CGameState::CGameState() {
 }
 
 void CGameState::begin() {
+   printf("Here in begin\n");
    Level = &CGameLevelLoader::loadLevel("jorge1.xml");
    GameplayManager = new CGameplayManager(*Level);
    DecorManager = new CDecorManager(*Level);
+   CCameraControl *Camera = new CCameraControl();
+   Camera->setPosition(SVector3(0, 0, 20));
+   Camera->setLookDirection(SVector3(0, 0, -1));
+   CApplication::get().getSceneManager().setActiveCamera(Camera);
+
+   CApplication::get().getSceneManager().Lights.push_back(new CLight());
+   CApplication::get().getSceneManager().Lights.back()->Color = SVector3(0.5f, 0.2f, 0.2f);
+   CApplication::get().getSceneManager().Lights.back()->Position = SVector3(1.f, 2.f, 3.f);
 }
 
 void CGameState::OnRenderStart(float const Elapsed) {
+   printf("Here in OnRenderStart\n");
    GameplayManager->update(Elapsed);
    DecorManager->update(Elapsed);
 }
