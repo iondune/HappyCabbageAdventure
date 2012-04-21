01d7d82a06d9ef05a7c4d14a7d6e091be38540fd
Alden Tai <xtoken.x@gmail.com>
2012-04-20 23:47:18 -0700

    have all of the hurt/normal cabbage meshes and renderables being added

--- CabbageGame/Elements/Player/CElementPlayer.cpp
+++ CabbageGame/Elements/Player/CElementPlayer.cpp
@@ -1,33 +1,29 @@
-#include "CElementBlock.h"
+#include "CElementPlayer.h"
 #include "CGameLevel.h"
 
-CElementBlock::CElementBlock(SRect2 nArea, int depth, int texture)
-: CGameplayElement(PhysicsEngineObject, SceneObject, nArea), Depth(depth), Texture(texture) {
+CElementPlayer::CElementPlayer(SRect2 nArea)
+: CGameplayElement(PhysicsEngineObject, SceneObject, nArea) {
 }
 
-void CElementBlock::updatePhysicsEngineObject(float time) {
+void CElementPlayer::updatePhysicsEngineObject(float time) {
 }
 
-void CElementBlock::updateSceneObject(float time) {
+void CElementPlayer::updateSceneObject(float time) {
 }
 
-void CElementBlock::OnCollision(CCollideable *Object, CCollideable *With) {
+void CElementPlayer::OnCollision(CCollideable *Object, CCollideable *With) {
    return;
 }
 
-void CElementBlock::writeXML(xmlwriter *l) {
-   std::stringstream xValue, yValue, widthValue, heightValue, tagValue, isMovingValue, rangeValue, speedValue, depthValue, textureType;
+void CElementPlayer::writeXML(xmlwriter *l) {
+   std::stringstream xValue, yValue, widthValue, heightValue, tagValue;
    xValue << Area.Position.X;
    yValue << Area.Position.Y;
    widthValue << Area.Size.X;
    heightValue << Area.Size.Y;
-   depthValue << Depth;
-   textureType << Texture;
 
-   tagValue << "CBlock";
+   tagValue << "CCabbage";
 
-   l->AddAtributes("texture ", textureType.str());
-   l->AddAtributes("depth ", depthValue.str());
    l->AddAtributes("width ", widthValue.str());
    l->AddAtributes("height ", heightValue.str());
    l->AddAtributes("Y ", yValue.str());
@@ -36,90 +32,83 @@ void CElementBlock::writeXML(xmlwriter *l) {
    l->CloseLasttag();
 }
 
-void CElementBlock::setupPhysicsEngineObject() {
-   /*
-    * Doing this in the consolidation phase in CGameState
-    * Thus, there will be no physics engine object that this can refer to! TODO: Check if this is good enough or not
-    CObject *engBlock = Engine->addObject();
-    engBlock->setArea(SRect2(x, y, Area.Size.X, Area.Size.Y));
-    */
+void CElementPlayer::setupPhysicsEngineObject() {
 }
 
-void CElementBlock::setupSceneObject() {
-   CMeshSceneObject *tempBlock = new CMeshSceneObject();
-   CMesh *mesh;
+#include <sstream>
+void CElementPlayer::setupSceneObject() {
+   SceneObject = new ISceneObject();
 
-   mesh = CMeshLoader::createCubeMesh();
-   mesh->calculateNormalsPerVertex();
-   tempBlock->setMesh(mesh);
-   if (Level.getEnvironment() == 0) {
-      switch(Texture) {
-      case 0:
-         tempBlock->setTexture("Base/grass.bmp");
-         break; 
-      case 1:
-         tempBlock->setTexture("Base/dirt.bmp");
-         break;
-      case 2:
-         tempBlock->setTexture("Base/rock.bmp");
-         break;
-      case -5:
-         mesh = CMeshLoader::load3dsMesh("Base/levelBlock.3ds");
-         if (mesh) {
-            mesh->resizeMesh(SVector3(1.0));
-            mesh->centerMeshByExtents(SVector3(0));
-            mesh->calculateNormalsPerVertex();
-         }
-         else {
-            fprintf(stderr, "Failed to load the mesh\n");
-         }
-         tempBlock->setMesh(mesh);
-         tempBlock->setTexture("Base/GrassyGrass.bmp", 2);
-         tempBlock->setTexture("Base/DirtyDirt.bmp", 3);
-         break;
-      default:
-         fprintf(stderr, "Unknown texture type %d\n", Texture);
-         break;
+   //Normal cabbage meshes and renderables
+   ISceneObject *NormalCabbage = new ISceneObject();
+   SceneObject->addChild(NormalCabbage);
+
+   for(int i = 1; i <= 5; i++) {
+      std::ostringstream fileName;
+      fileName << "Base/cabbage/cabbage_" << i << ".3ds";
+      CMesh *cabbageMesh = CMeshLoader::load3dsMesh(fileName.str());
+      if(cabbageMesh) {
+         cabbageMesh->centerMeshByExtents(SVector3(0.0f));
+         cabbageMesh->calculateNormalsPerFace();
+         cabbageMesh->resizeMesh(SVector3(0.45f));
       }
+
+      CMeshSceneObject *cabbageRenderable = new CMeshSceneObject();
+      cabbageRenderable->setMesh(cabbageMesh);
+      cabbageRenderable->setShader(ERP_DEFAULT, "Toon");
+      cabbageRenderable->setShader(ERP_DEFERRED_OBJECTS, "Deferred/Toon");
+      cabbageRenderable->setCullingEnabled(false);
+      cabbageRenderable->setVisible(Stats.Health == i);
+
+      NormalCabbage->addChild(cabbageRenderable);
    }
-   else if (Level.getEnvironment() == 1) {
-      switch(Texture) {
-      case -5:
-         mesh = CMeshLoader::load3dsMesh("Base/levelBlock.3ds");
-         if (mesh) {
-            mesh->resizeMesh(SVector3(1.0));
-            mesh->centerMeshByExtents(SVector3(0));
-            mesh->calculateNormalsPerVertex();
-         }
-         else {
-            fprintf(stderr, "Failed to load the mesh\n");
-         }
-         tempBlock->setMesh(mesh);
-         tempBlock->setTexture("Base/DesertyDesert.tga", 2);
-         tempBlock->setTexture("Base/DirtyDirt.bmp", 3);
-         break;
-      case 0:
-      case 1:
-      case 2:
-         tempBlock->setTexture("Base/desert.bmp");
-         break;
-      default:
-         fprintf(stderr, "Unknown texture type %d\n", Texture);
-         break;
+
+   //Hurt cabbage meshes and renderables
+   ISceneObject *HurtCabbage = new ISceneObject();
+   SceneObject->addChild(HurtCabbage);
+
+   for(int i = 1; i <= 5; i++) {
+      std::ostringstream fileName;
+      fileName << "Base/cabbage/cabbage_ouch_" << i << ".3ds";
+      CMesh *cabbageMesh = CMeshLoader::load3dsMesh(fileName.str());
+      if(cabbageMesh) {
+         cabbageMesh->centerMeshByExtents(SVector3(0.0f));
+         cabbageMesh->calculateNormalsPerFace();
+         cabbageMesh->resizeMesh(SVector3(0.85f));
       }
-   }
-   tempBlock->setShader(ERP_DEFAULT, CShaderLoader::loadShader("ToonTexture"));
-   tempBlock->setShader(ERP_DEFERRED_OBJECTS, CShaderLoader::loadShader("Deferred/Textured"));
-   if(Texture != -5) {
-      tempBlock->setTranslation(SVector3((Area.Position.X+(Area.Position.X+Area.Size.X))/2, (Area.Position.Y+(Area.Position.Y+Area.Size.Y))/2, 0));
-      tempBlock->setScale(SVector3(Area.Size.X, Area.Size.Y, (float) Depth));
-   }
-   else /* For ground blocks */{
-      tempBlock->setTranslation(SVector3((Area.Position.X+(Area.Position.X+Area.Size.X))/2, Area.Position.Y+.84f*Area.Size.Y, 0));
-      tempBlock->setScale(SVector3(Area.Size.X, (float) Depth, Area.Size.Y)); //Area.Size.X, Area.Size.Y, Depth
+
+      CMeshSceneObject *cabbageRenderable = new CMeshSceneObject();
+      cabbageRenderable->setMesh(cabbageMesh);
+      cabbageRenderable->setShader(ERP_DEFAULT, "Toon");
+      cabbageRenderable->setShader(ERP_DEFERRED_OBJECTS, "Deferred/Toon");
+      cabbageRenderable->setCullingEnabled(false);
+      cabbageRenderable->setVisible(false);
+      cabbageRenderable->setTranslation(SVector3(0.0f, 0.0f, 0.2f));
+
+      HurtCabbage->addChild(cabbageRenderable);
    }
 
-   tempBlock->setRotation(SVector3(Texture==-5?-90.f:0, 0, 0));
+   ////
+   //PlayerView = new CPlayerView();
 
-   CApplication::get().getSceneManager().addImmobileSceneObject(tempBlock, THIS_OBJECT_WILL_NEVER_MOVE_AND_ITS_BOUNDING_BOX_IS_CORRECT);
+   /* SHADOW
+   ISceneObject *CabbageShadow = new ISceneObject();
+   SceneObject->addChild(CabbageShadow);
+
+   CMeshSceneObject *renderLeftShadow = new CMeshSceneObject();
+   renderLeftShadow->setMesh(discMesh);
+   renderLeftShadow->setShader(ERP_DEFAULT, BlackShader);
+   renderLeftShadow->setShader(ERP_DEFERRED_OBJECTS, BlackShader);
+   renderLeftShadow->addUniform("cutoff", PlayerView->LeftShadowCutoffValue);
+   renderLeftShadow->addUniform("start", PlayerView->LeftShadowStartValue);
+
+   CMeshSceneObject *renderRightShadow = new CMeshSceneObject();
+   renderRightShadow->setMesh(discMesh);
+   renderRightShadow->setShader(ERP_DEFAULT, BlackShader);
+   renderRightShadow->setShader(ERP_DEFERRED_OBJECTS, BlackShader);
+   renderRightShadow->addUniform("cutoff", PlayerView->RightShadowCutoffValue);
+   renderRightShadow->addUniform("start", PlayerView->RightShadowStartValue);
+
+   CApplication::get().getSceneManager().addSceneObject(CabbageShadow);
+   */
 }
--- CabbageGame/Elements/Player/CElementPlayer.h
+++ CabbageGame/Elements/Player/CElementPlayer.h
@@ -1,14 +1,51 @@
-#ifndef __CBLOCK_H____
-#define __CBLOCK_H____
+#ifndef __CPLAYER_H____
+#define __CPLAYER_H____
 
 #include "../CGameplayElement.h"
+#include "CEventManager.h"
 
-class CElementBlock : public CGameplayElement {
+#define NUM_CABBAGE_MESH_STATES 5
+
+namespace Cabbage {
+   //Information kept between levels
+   class PlayerInformation {
+      public:
+         int Health;
+         int Energy;
+         int Lives;
+         int Seeds;
+
+         PlayerInformation() : Health(5), Energy(3), Lives(3), Seeds(0) {
+
+         }
+   };
+
+   //Information kept in the Overworld
+   class LevelInformation {
+      public:
+         int LevelsCompleted;
+         int LastLevelEntered;
+
+         LevelInformation() : LevelsCompleted(0), LastLevelEntered(0) {
+
+         }
+   };
+}
+
+class CElementPlayer : public CGameplayElement, public CApplicationEventReceiver {
    private:
       CCollisionObject * PhysicsEngineObject; //Override CGameplayElement's but with type CObject
-      CMeshSceneObject * SceneObject; //Override CGameplayElement's but with type CMeshSceneObject
-      int Depth, Texture;
+      ISceneObject * SceneObject; //Override CGameplayElement's. This will contain the cabbage and its shadow 
+
+      ISceneObject HurtCabbageArr[NUM_CABBAGE_MESH_STATES];
+      ISceneObject NormalCabbageArr[NUM_CABBAGE_MESH_STATES];
+
+      Cabbage::PlayerInformation Stats;
+
    public:
+      CElementPlayer(SRect2 nArea);
+
+      //CGameplayElement functions
       virtual void OnCollision(CCollideable *Object, CCollideable *With);
 
       virtual void setupPhysicsEngineObject();
@@ -18,7 +55,9 @@ class CElementBlock : public CGameplayElement {
       virtual void updateSceneObject(float);
 
       virtual void writeXML(xmlwriter *);
-      CElementBlock(SRect2 nArea, int depth, int texture);
+
+      //Keyboard event functions
+      //void OnKeyboardEvent(SKeyboardEvent const & Event);
 };
 
 #endif
