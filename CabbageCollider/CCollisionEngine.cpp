23ed7631e71c219a2c930f984d88701eaadfe8e1
Ian <Ian@localhost>
2012-04-25 18:39:24 -0700

    + Fixed release build of collider demo

--- CabbageCollider/CCollisionEngine.cpp
+++ CabbageCollider/CCollisionEngine.cpp
@@ -45,7 +45,7 @@ void CCollisionEngine::performTick(float const TickTime)
 
 	for (ObjectList::iterator it = Objects.begin(); it != Objects.end(); ++ it)
 	{
-		SVector2<float> Movement = (* it)->performMovement(TickTime);
+		SVector2<double> Movement = (* it)->performMovement(TickTime);
 
 		for (ActorList::iterator jt = Actors.begin(); jt != Actors.end(); ++ jt)
 		{
