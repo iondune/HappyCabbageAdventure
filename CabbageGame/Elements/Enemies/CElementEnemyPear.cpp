b5deba408630b78ecc032099dd5ad9905dfb1188
X <X@localhost>
2012-05-02 23:50:00 -0700

    Forgot to add classes.  Added Cherry classes too.

---
+++ CabbageGame/Elements/Enemies/CElementEnemyPear.cpp
@@ -0,0 +1,85 @@
+#include "CElementEnemyPear.h"
+#include "CGameLevel.h"
+
+CElementEnemyPear::CElementEnemyPear(SRect2 nArea) :
+   CElementEnemy(nArea, Enemies::PEAR), ISquishable(nArea.Size.X, nArea.Size.Y) {
+
+}
+
+void CElementEnemyPear::setupPhysicsEngineObject() {
+   /* Set up the actor (not actually an actor, since this one doesn't move its position) */
+   PhysicsEngineObject = Level.getPhysicsEngine().addActor();
+   PhysicsEngineObject->setArea(Area);
+
+   //Set actor attributes
+   PhysicsEngineObject->getAttributes().MaxWalk = 2.2f;
+   PhysicsEngineObject->CollideableType = COLLIDEABLE_TYPE_PEAR;
+}
+
+void CElementEnemyPear::setupSceneObject() {
+   SceneObject = new CMeshSceneObject();
+   CMesh *mesh;
+
+   if (Level.getEnvironment() == 0) {
+      mesh = CMeshLoader::load3dsMesh("Base/pear.3ds");
+   }
+
+   else if (Level.getEnvironment() == 1) {
+      mesh = CMeshLoader::load3dsMesh("Base/pear.3ds");
+   }
+
+   if(mesh) {
+      mesh->resizeMesh(SVector3(1));
+      mesh->centerMeshByExtents(SVector3(0));
+      mesh->calculateNormalsPerFace();
+   }
+
+   else
+      printf("ERROR.  MESH DID NOT LOAD PROPERLY.\n");
+
+   SceneObject->setMesh(mesh);
+   SceneObject->setShader(ERP_DEFAULT, "Toon");
+   SceneObject->setShader(ERP_DEFERRED_OBJECTS, "Deferred/Toon");
+   SceneObject->setTranslation(SVector3((Area.Position.X+(Area.Position.X+1))/2, (Area.Position.Y+(Area.Position.Y-1))/2, 0));
+   SceneObject->setScale(SVector3(Area.Size.X, Area.Size.X, Area.Size.Y));
+   SceneObject->setRotation(SVector3(-90, 0, 0));
+
+   CApplication::get().getSceneManager().addSceneObject(SceneObject);
+}
+
+//CGameplayElement has an attribute called ElapsedTime, which is updated by CGameplayElement's update function.
+
+//This is where the AI would be updated for more complex enemies
+void CElementEnemyPear::updatePhysicsEngineObject(float time) {
+   PhysicsEngineObject->setAction(CCollisionActor::EActionType::MoveLeft);
+}
+
+//This is where the renderable would be updated for the more complex enemies
+void CElementEnemyPear::updateSceneObject(float time) {
+   SceneObject->setTranslation(SVector3(Area.getCenter().X,Area.getCenter().Y, 0));
+   if(ParticleEngine) {
+      SceneObject->setTranslation(SVector3(Area.getCenter().X, Area.Position.Y, 0));
+      SceneObject->setRotation(SVector3(-90, 0, 0));
+      SceneObject->setScale(SVector3(1.0f, 1.0f, 0.3f));
+      return;
+   }
+
+   Scale = ISquishable::Squish(PhysicsEngineObject->getVelocity());
+
+   if (PhysicsEngineObject->getVelocity().Y < .01f && PhysicsEngineObject->getVelocity().Y > -.01f) {
+      if(PhysicsEngineObject->getVelocity().X < -0.01f)
+         SceneObject->setScale(SVector3(-Scale.X,Scale.X,Scale.Y));
+      else if(PhysicsEngineObject->getVelocity().X > 0.01f)
+         SceneObject->setScale(SVector3(Scale.X,Scale.X,Scale.Y));
+   }
+   else {
+      if(PhysicsEngineObject->getVelocity().X < -0.01f)
+         SceneObject->setScale(SVector3(-1,1,1));
+      else if(PhysicsEngineObject->getVelocity().X > 0.01f)
+         SceneObject->setScale(SVector3(1,1,1));
+   }
+}
+
+void CElementEnemyPear::printInformation() {
+   printf("CElementEnemyPear; Area: [[%0.0f, %0.0f],[%0.0f, %0.0f]]\n", Area.Position.X, Area.Position.Y, Area.Size.X, Area.Size.Y);
+}
---
+++ CabbageGame/Elements/Enemies/CElementEnemyPear.h
@@ -0,0 +1,23 @@
+#ifndef CELEMENTENEMYPear_H_
+#define CELEMENTENEMYPear_H_
+
+#include "CElementEnemy.h"
+
+class CElementEnemyPear : public CElementEnemy, public ISquishable
+{
+   private:
+      SVector2 Scale;
+
+   public:
+      CElementEnemyPear(SRect2 nArea);
+
+      virtual void setupPhysicsEngineObject();
+      virtual void setupSceneObject();
+
+      virtual void updatePhysicsEngineObject(float time);
+      virtual void updateSceneObject(float time);
+
+      virtual void printInformation();
+};
+
+#endif
