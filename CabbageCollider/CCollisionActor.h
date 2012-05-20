24907e5bf7bba2cf6eaf62a93fe841470d0d0580
Ian <Ian@localhost>
2012-05-20 06:27:46 -0700

    + Implemented Actor as Interactor

--- CabbageCollider/CCollisionActor.h
+++ CabbageCollider/CCollisionActor.h
@@ -1,20 +1,16 @@
 #ifndef _CABBAGECOLLIDER_CACTOR_H_INCLUDED_
 #define _CABBAGECOLLIDER_CACTOR_H_INCLUDED_
 
-#include <SRect2.h>
-#include <vector>
+#include "CCollisionInteractor.h"
 
-#include "CCollideable.h"
-
-class CCollisionObject;
-
-class CCollisionActor : public CCollideable
+class CCollisionActor : public CCollisionInteractor
 {
 
 public:
 
-	struct SAttributes
+	struct SAttributes : public CCollisionInteractor::SAttributes
 	{
+
 		//! Initial acceleration given during a jump
 		CollisionReal JumpAccel;
 		
@@ -33,23 +29,9 @@ public:
 		//! Degree of movement control while falling (factors into acceleration)
 		CollisionReal AirSpeedFactor;
 
-		//! Rate at which horizontal velocity decreases while falling
-		CollisionReal AirStandingFriction;
-
-		//! Rate at which horizontal velocity decreases while falling
-		CollisionReal GroundStandingFriction;
-
-		//! Ratio of velocity reflected during collisions
-		CollisionReal Bounce;
-
-		//! Ratio of gravity affecting this object
-		CollisionReal GravityMultiplier;
-
-		//! Maximum step up for actor
-		CollisionReal MaxStep;
-
 		//! Default params ctor
 		SAttributes();
+
 	};
 
 	//! Action type of this object, defines what movement the Actor is trying to perform
@@ -80,14 +62,6 @@ public:
 
 protected:
 
-	friend class CCollisionEngine;
-
-	//! Current velocity of this actor
-	SVec2 Velocity;
-
-	//! While standing, points to the object being stood on
-	CCollideable * Standing;
-
 	//! Behavior attributes of this actor
 	SAttributes Attributes;
 
@@ -101,29 +75,12 @@ protected:
 	bool Jumping;
 	bool WantsToJump;
 
-	//! Sets whether this actor should be accelerated by gravity
-	bool GravityEnabled;
-
-	//! Acceleration due to falling
-	CollisionReal FallAcceleration;
-
-	//! Impulse movement!
-	std::vector<std::pair<SVec2, float> > Impulses;
-
 	//! For debugging purposes, is true when the last update tick used an "allowed collision" movement
 	bool AllowedMovement;
 
-	//! Position held by this actor during last tick
-	SVec2 LastPosition;
-
-	//! Movement of this actor during current tick
-	SVec2 Movement;
-
-	//! Lists maintaining which objects are currently phased through this object
-	std::set<CCollideable *> PhaseList, NewPhaseList;
-
 
 	//! Constructor
+	friend class CCollisionEngine;
 	CCollisionActor(CCollisionEngine * collisionEngine);
 
 
@@ -134,48 +91,37 @@ protected:
 	//! Updates the movement vectors of this actor
 	//! Called once per tick
 	//! Called first before initial update!
-	void updateVectors(CollisionReal const TickTime);
+	virtual void updateVectors(CollisionReal const TickTime);
 
 	//! Peforms a collision check between this actor and a collideable object
 	//! Returns true if a standing event occured
 	//! 
-	bool updateCollision(CCollideable * Object, float const TickTime);
+	virtual bool updateCollision(CCollideable * Object, float const TickTime);
 
 	//! Performs a discrete collision check between this actor and a collideable object
 	//! Called by updateCollision
 	//! Returns the direction collisions that occured, if any
 	//! Adjusts movement to prevent collision
-	int checkCollision(CCollideable * Object, CollisionReal const TickTime);
+	virtual int checkCollision(CCollideable * Object, CollisionReal const TickTime);
 
 	//! Called by updateCollision when a downwards collision occurs
-	void onStanding(CCollideable * Object);
+	virtual void onStanding(CCollideable * Object);
 
 	//! Moves this object if a moveable Object caused a collision and push
-	void pushIfCollided(CCollisionObject * Object, SVec2 const Movement);
+	virtual void pushIfCollided(CCollisionObject * Object, SVec2 const Movement);
 
 	//! After update, sends event responses for objects which left phase state
-	void updatePhaseList();
+	virtual void updatePhaseList();
 
 public:
 
 	//! Destructor
 	~CCollisionActor();
 
-	//! Checks if this actor is directly above an object, and if so, returns the height between them
-	bool isAbove(CCollisionObject * Object, float & height) const;
-
-	//! See Standing
-	bool const isStanding() const;
-
-	//! See Velocity
-	void setVelocity(SVec2 const & vel);
-	//! See Velocity
-	SVec2 const & getVelocity() const;
-
 	//! See Attributes
-	SAttributes const & getAttributes() const;
+	SAttributes const & getActorAttributes() const;
 	//! See Attributes
-	SAttributes & getAttributes();
+	SAttributes & getActorAttributes();
 
 	//! Sets the current movement action of this Actor
 	void setAction(EActionType const & action);
@@ -187,16 +133,8 @@ public:
 	//! Used to set jumping action
 	void setJumping(bool const jumping);
 
-	//! Sets whether this object is affected by Gravity
-	void setGravityEnabled(bool const gravityEnabled);
-	//! Gets if this object is affected by Gravity
-	bool const isGravityEnabled() const;
-
 	//! For debugging purposes, draws this actor onto the screen using FFP OpenGL
 	virtual void draw();
-
-	//! Adds an impulse to this Actor
-	void addImpulse(SVec2 const & velocity, float const Duration = 0.001f);
 
 };
 
