c35c4896ffa9b16b9c41f849f873aa775b604894
iondune <iondune@localhost>
2012-05-27 18:18:28 -0700

    + Unreferenced CMaterial (moved to CRenderable)

--- CabbageScene/CMaterial.h
+++ CabbageScene/CMaterial.h
@@ -1,10 +1,1 @@
-#ifndef _CABBAGE_SCENE_CMATERIAL_H_INCLUDED_
-#define _CABBAGE_SCENE_CMATERIAL_H_INCLUDED_
-
-#include "SUniform.h"
-#include "SAttribute.h"
-#include "CTexture.h"
-
-
-
-#endif
+#deprecated
