018ec84a61bd61098a94375a798189d86a67f52f
iondune <iondune@localhost>
2012-06-03 04:07:56 -0700

    + Fixed name of CPerspectiveCameraSceneObject

--- CabbageScene/CPerspectiveCameraSceneObject.h
+++ CabbageScene/CPerspectiveCameraSceneObject.h
@@ -4,12 +4,12 @@
 #include "CCameraSceneObject.h"
 
 
-class CPerspectiveCamera : public CCameraSceneObject
+class CPerspectiveCameraSceneObject : public CCameraSceneObject
 {
 
 public:
 
-    CPerspectiveCamera(float const AspectRatio = 16.f/9.f, float const Near = 0.1f, float const Far = 100.f, float const FOV = 60.f);
+    CPerspectiveCameraSceneObject(float const AspectRatio = 16.f/9.f, float const Near = 0.1f, float const Far = 100.f, float const FOV = 60.f);
 
     virtual void setProjection(float const FOV, float const AspectRatio, float const Near, float const Far);
 
